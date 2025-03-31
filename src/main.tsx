253633e8b1ac35cd8092e2d1bcbcc807fee7479a
anshif <anshif.pk@zartek.in>
2025-03-31 20:02:32 +0530

    Fix Router basename to match GitHub Pages URL

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,7 @@ import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <Router basename="/anshif-portfolio">
+    <Router basename="/profile">
       <App />
     </Router>
   </React.StrictMode>,
